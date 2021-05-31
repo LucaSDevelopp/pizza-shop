9c1be1a3ab7e61d98d0b829a319635c239ca3541
= <=>
2021-05-31 05:34:26 -0700

    food items optimized for mobile view

--- components/foods/Food-item/FoodItem.js
+++ components/foods/Food-item/FoodItem.js
@@ -4,21 +4,19 @@ import Image from "next/image";
 const FoodItem = (props) => {
 	const exploreLink = `/menu/[pizzaMenuId]/${props.id}`;
 	return (
-		<li className={classes.food}>
-			<Link href={exploreLink}>
-				<div className={classes.link}>
-					<h2 className={classes.title}>{props.name}</h2>
-					<div className={classes.image}>
-						<Image
-							src={props.icon}
-							alt={props.name}
-							width="auto"
-							height="auto"
-						/>
-					</div>
+		<Link href={exploreLink}>
+			<div className={classes.link}>
+				<h2 className={classes.title}>{props.name}</h2>
+				<div className={classes.image}>
+					<Image
+						src={props.icon}
+						alt={props.name}
+						width="120px"
+						height="75px"
+					/>
 				</div>
-			</Link>
-		</li>
+			</div>
+		</Link>
 	);
 };
 
